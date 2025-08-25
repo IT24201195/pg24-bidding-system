d0da8703a473ea278afd248cac6e630664755a7d
IT24201195 <it24201195@my.sliit.lk>
2025-08-25 13:24:37 +0530

    chore: project setup

---
+++ backend/bidding/src/test/java/com/pg24/bidding/BiddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pg24.bidding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
