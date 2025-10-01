8178956d78f4eccd4884a5386169109efd8efae4
IT24104304 <IT24104304@my.sliit.lk>
2025-10-01 12:39:03 +0530

    Nelundi : Auction exception.java

---
+++ backend/bidding/src/main/java/com/pg24/bidding/finalize/service/exception/BidNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.Bidding.System.exception;
+
+public class BidNotFoundException extends RuntimeException {
+    public BidNotFoundException(String message) {
+        super(message);
+    }
+
+    public BidNotFoundException(Long bidId) {
+        super("Bid not found with id: " + bidId);
+    }
+}
